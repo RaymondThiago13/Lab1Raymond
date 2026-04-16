59a681554437ea732ee698355c0f5c33f91358ac
RaymondThiago13 <tnraymond2003@gmail.com>
2026-04-16 19:34:00 +0200

    Getting started

---
+++ app/src/main/java/com/example/lab1raymond/Routes/Routes.kt
@@ -0,0 +1,5 @@
+package com.example.lab1raymond.Routes
+
+object Routes {
+
+}
---
+++ app/src/main/java/com/example/lab1raymond/models/Movie.kt
@@ -0,0 +1,17 @@
+package com.example.lab1raymond.models
+
+data class Movie(
+    var id: Long = 0L,
+    var imdbId: String,
+    var title: String,
+    var releaseDate: String,
+    var genres: List<String>,
+    var overview: String,
+    var homepageUrl: String,
+    var posterPath: String,
+    var backdropPath: String
+){
+    fun getImdbUrl(): String{
+        return "https://www.imdb.com/title/$imdbId/"
+    }
+}
