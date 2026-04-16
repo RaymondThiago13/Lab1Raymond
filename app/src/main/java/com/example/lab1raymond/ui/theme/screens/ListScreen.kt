99155f675a1180c0c9a6244c2be295f28ea70543
RaymondThiago13 <tnraymond2003@gmail.com>
2026-04-17 01:05:49 +0200

    LastUpdate!

--- app/src/main/java/com/example/lab1raymond/ui/theme/screens/ListScreen.kt
+++ app/src/main/java/com/example/lab1raymond/ui/theme/screens/ListScreen.kt
@@ -1,11 +1,14 @@
 package com.example.lab1raymond.ui.theme.screens
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
@@ -13,9 +16,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil3.compose.AsyncImage
 import com.example.lab1raymond.models.Movie
+import com.example.lab1raymond.utils.Constants
 
 @Composable
 fun ListScreen(
@@ -35,18 +41,31 @@ fun MovieItem(movie: Movie, navController: NavController){
     Card(
         modifier = Modifier.padding(8.dp).fillMaxWidth().clickable{ navController.navigate("details/${movie.id}") }
     ){
-        Column(modifier = Modifier.padding(16.dp)){
-            Text(
-                text = movie.title,
-                style = MaterialTheme.typography.titleLarge
-            )
+        Row{
+            Box{
+                AsyncImage(
+                    model = Constants.POSTER_IMAGE_BASE_URL
+                            + Constants.POSTER_IMAGE_BASE_WIDTH
+                            + movie.posterPath,
+                    contentDescription = movie.title,
+                    modifier = Modifier.width(92.dp).height(138.dp),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Column(modifier = Modifier.padding(16.dp)){
+                Text(
+                    text = movie.title,
+                    style = MaterialTheme.typography.titleLarge
+                )
 
-            Spacer(modifier = Modifier.height(8.dp))
+                Spacer(modifier = Modifier.height(8.dp))
 
-            Text(
-                text = movie.overview,
-                maxLines = 2
-            )
+                Text(
+                    text = movie.overview,
+                    maxLines = 2
+                )
+            }
         }
+
     }
 }
