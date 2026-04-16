e3d568301278629bf477c3898a53cb7fca14fee5
RaymondThiago13 <tnraymond2003@gmail.com>
2026-04-16 20:43:45 +0200

    Advance2

--- app/src/main/java/com/example/lab1raymond/MainActivity.kt
+++ app/src/main/java/com/example/lab1raymond/MainActivity.kt
@@ -6,11 +6,19 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -74,6 +82,39 @@ fun runApp() {
             }
         }
     }
+}
+
+@Composable
+fun MyTopBar(navController: NavHostController){
+    CenterAlignedTopAppBar(
+        title = {
+            Text("My Movie App")
+        },
+
+        //NavigationIcon stands for the button on the left.
+        navigationIcon = {
+            IconButton(onClick = {
+                navController.navigate(Routes.LIST){ popUpTo(Routes.LIST) }
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = "Home"
+                )
+            }
+        },
+
+        //actions stand for the button on the right.
+        actions = {
+            IconButton(onClick = {
+                navController.navigate(Routes.INFO)
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Info,
+                    contentDescription = "Info"
+                )
+            }
+        }
+    )
 }
 
 @Preview(showBackground = true)
