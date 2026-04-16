5c354e77a9a398d4f7eb5c94e0ea1592ef5fc233
RaymondThiago13 <tnraymond2003@gmail.com>
2026-04-16 19:59:17 +0200

    Advance1

--- app/src/main/java/com/example/lab1raymond/MainActivity.kt
+++ app/src/main/java/com/example/lab1raymond/MainActivity.kt
@@ -11,6 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.lab1raymond.Routes.Routes
+import com.example.lab1raymond.database.Movies
 import com.example.lab1raymond.ui.theme.Lab1RaymondTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +26,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Lab1RaymondTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                runApp()
                 }
             }
         }
@@ -31,11 +34,46 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun runApp() {
+    val navController = rememberNavController()
+
+    Scaffold(
+        topBar = { MyTopBar (navController) }
+    ) {innerpadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Routes.LIST
+        ){
+            composable(Routes.LIST){
+                ListScreen(
+                    movies = Movies.getMovies(),
+                    navController = navController,
+                    modifier = Modifier.padding(innerpadding)
+                )
+            }
+
+            composable(
+                route = Routes.Detail,
+                arguments = listOf(navArgument("movieId") {type = NavType.LongType})
+            ){
+                backStackEntry ->
+                val movieId = backStackEntry.arguments?.getLong("movieId")
+
+                val movie = Movies.getMovies().find { it.id == movieId }
+
+                if(movie != null){
+                    DetailScreen(
+                        movie = movie,
+                        modifier = Modifier.padding(innerpadding)
+                    )
+                }
+            }
+
+            composable(Routes.INFO){
+                infoScreen(modifier = Modifier.padding(innerpadding))
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/lab1raymond/Routes/Routes.kt
+++ app/src/main/java/com/example/lab1raymond/Routes/Routes.kt
@@ -1,5 +1,7 @@
 package com.example.lab1raymond.Routes
 
 object Routes {
-
+    const val LIST = "list"
+    const val Detail = "detail/{movieId}"
+    const val INFO = "info"
 }
